4c715fa5bb5d05ffd9980e7dae453150901deb1e
linyu <mi-cheng@outlook.com>
2024-02-13 21:34:00 +0800

    code before chapter 4.3

--- main.cpp
+++ main.cpp
@@ -12,6 +12,14 @@ using std::cout;	using std::string;
 using std::endl;	using std::streamsize;
 using std::vector;	using std::sort;
 using std::istream;	using std::domain_error;
+using std::max;
+
+struct Student_info {
+	string name;
+	double midterm, final;
+	vector<double> homework;
+}; // note the semicolon -- it's required
+
 
 // compute the median of a vector<double>
 // note that calling this function copies the entire argument vector
@@ -42,6 +50,11 @@ double grade(double midterm, double final, const vector<double>& hw)
 	return grade(midterm, final, median(hw));
 }
 
+double grade(const Student_info& s)
+{
+	return grade(s.midterm, s.final, s.homework);
+}
+
 // read homework grades from an input stream into a vector<double>
 istream& read_hw(istream& in, vector<double>& hw)
 {
@@ -60,40 +73,55 @@ istream& read_hw(istream& in, vector<double>& hw)
 	return in;
 }
 
+istream& read(istream& is, Student_info& s)
+{
+	// read and store the student's name and midterm and final exam grades
+	is >> s.name >> s.midterm >> s.final;
+
+	read_hw(is, s.homework); // read and store all the student's homework grades
+	return is;
+}
+
+bool compare(const Student_info& x, const Student_info& y)
+{
+	return x.name < y.name;
+}
+
+
 int main()
 {
-	// ask for and read the student's name
-	cout << "Please enter your first name: ";
-	string name;
-	cin >> name;
-	cout << "Hello, " << name << "!" << endl;
+	vector<Student_info> students;
+	Student_info record;
+	string::size_type maxlen = 0;
 
-	// ask for and read the midterm and final grades
-	cout << "Please enter your midterm and final exam grades: ";
-	double midterm, final;
-	cin >> midterm >> final;
-
-	// ask for the homework grades
-	cout << "Enter all your home work grades, "
-		"followed by end-of-file: ";
-
-	vector<double> homework;
-
-	// read the homework gradesa
-	read_hw(cin, homework);
-
-	// compute and generate the final grade, if possible
-	try {
-		double final_grade = grade(midterm, final, homework);
-		streamsize prec = cout.precision();
-		cout << "Your final grade is " << setprecision(3)
-			<< final_grade << setprecision(prec) << endl;
+	// read and store all the records, and find the length of the longest name
+	while (read(cin, record)) {
+		maxlen = max(maxlen, record.name.size());
+		students.push_back(record);
 	}
-	catch (domain_error) {
-		cout << endl << "You must enter your grades. "
-			"Please try again." << endl;
+
+	// alphabetize the records
+	sort(students.begin(), students.end(), compare);
+
+	for (vector<Student_info>::size_type i = 0;
+		i != students.size(); ++i) {
+
+		// write the name,padded on the right to maxlen + 1 characters
+		cout << students[i].name
+			<< string(maxlen + 1 - students[i].name.size(), ' ');
+
+		// compute and write the grade
+		try {
+			double final_grade = grade(students[i]);
+			streamsize prec = cout.precision();
+			cout << setprecision(3) << final_grade
+				<< setprecision(prec);
+		}
+		catch (domain_error e) {
+			cout << e.what();
+		}
+		cout << endl;
 	}
 	
 	return 0;
 }
-
